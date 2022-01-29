23046e4bebdbe17918f33f37bd8825a7de5667b4
bkata64 <64857455+bkata64@users.noreply.github.com>
2022-01-29 23:26:35 +0100

    GUI start

--- amoba_GUI.py
+++ amoba_GUI.py
@@ -1,11 +1,26 @@
 import tkinter as tk
 
+def onclick(event):
+    global player
+    if player == 1:
+        event.widget["text"] = "X"
+        player = 0
+    else:
+        event.widget["text"] = "O"
+        player = 1
+
+n = 10
 window = tk.Tk()
-window.rowconfigure(list(range(10)),  weight=1)
-window.columnconfigure(list(range(10)), weight=1)
-for i in range(10):
-    for j in range(10):
-        cimke = tk.Label(text="", borderwidth=1, relief="solid")
-        cimke.grid(row=i, column=j, ipadx=20, ipady=15)
+window.title("Amőba")
+window.geometry("400x400")
+window.rowconfigure(list(range(n)),  weight=1)
+window.columnconfigure(list(range(n)), weight=1)
+for i in range(n):
+    for j in range(n):
+        cimke = tk.Label(text="  ", borderwidth=1, relief="solid", font=("Arial", 16))
+        cimke.bind("<Button-1>", onclick)        
+        cimke.place(relwidth= 1/n, relheight=1/n, x=i*40, y=j*40)
+
+player = 1
 
 window.mainloop()
